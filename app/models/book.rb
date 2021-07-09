748d736ad0686449579bb08690c02ae792c23564
yoona610 <xiaotengxinai@gmail.com>
2021-07-09 09:49:44 +0000

    応用課題7b

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,15 +2,15 @@ class Book < ApplicationRecord
 	belongs_to :user
 	has_many :book_comments, dependent: :destroy
 	has_many :favorites, dependent: :destroy
+	validates :title, presence: true
+	validates :body, presence: true, length: {maximum: 200}
 
 	def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
 
-  def self.last_week
-  	Book.joins(:favorites).where(favorites: {created_at: 0.days.ago.prev_week..0.days.ago.prev_week(:sunday)}).group(:id).order("count(*) desc")
-  end
-
-	validates :title, presence: true
-	validates :body, presence: true, length: {maximum: 200}
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day)}
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day)}
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day)}
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day)}
 end
